e242346b50ffada9a65e9af8c548970027236726
Kathy <penguin.samba.rumba@gmail.com>
2018-03-11 19:23:24 +1300

    Update clsReport.cs

--- clsReport.cs
+++ clsReport.cs
@@ -1,70 +1,69 @@
-﻿using System.Collections.Generic;
-using System.Data;
+    using System.Data;
+    using System.Collections.Generic;
 
-public class clsReport
-{
-    public string Fname { get; set; }
-    public string Lname { get; set; }
-    public string Tname { get; set; }
-    public string Room { get; set; }
-    public string Ginfo { get; set; }
-    public string RptDate { get; set; }
-    public string AbsDate { get; set; }
-    public string Reason { get; set; }
-
-    public clsReport(){ }
-    
-    public clsReport(string fname, string lname, string tname, string room, string ginfo, string rptdate, string absdate, string reason)
+    public class ClsReport
     {
-        Fname = fname;
-        Lname = lname;
-        Tname = tname;
-        Room = room;
-        Ginfo = ginfo;
-        RptDate = rptdate;
-        AbsDate = absdate;
-        Reason = reason;
-    }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Tname { get; set; }
+        public string Room { get; set; }
+        public string Ginfo { get; set; }
+        public string RptDate { get; set; }
+        public string AbsDate { get; set; }
+        public string Reason { get; set; }
 
-    //This method returns today's absentee information
-    public List<clsRept> getAbsenteeToday(string room)
-    {
-        List<clsRept> rptInfo = new List<clsRept>();
+        public ClsReport() { }
 
-        IStorageAgent agt = new clsStorageAgent();
-        var con = agt.GetStorageConnection();
-        DataTable dtAbsentee = agt.GetData(con, "sp_select_absentee_today");
-
-
-        if (dtAbsentee != null)
+        public ClsReport(string fname, string lname, string tname, string room, string ginfo, string rptdate, string absdate, string reason)
         {
-            foreach (DataRow r in dtAbsentee.Rows)
+            Fname = fname;
+            Lname = lname;
+            Tname = tname;
+            Room = room;
+            Ginfo = ginfo;
+            RptDate = rptdate;
+            AbsDate = absdate;
+            Reason = reason;
+        }
+
+        //This method returns today's absentee information
+        public List<ClsReport> getAbsenteeToday(string room)
+        {
+            List<ClsReport> rptInfo = new List<ClsReport>();
+
+            ClsStorageAgent agt = new ClsStorageAgent();
+            var con = agt.GetStorageConnection();
+            DataTable dtAbsentee = agt.GetData(con, "sp_select_absentee_today");
+
+
+            if (dtAbsentee != null)
             {
-                string fname = r[1].ToString();
-                string lname = r[2].ToString();
-                string tname = r[3].ToString();
-                string rm = r[4].ToString();
-                string ginfo = r[5].ToString();
-                string rptDate = r[6].ToString();
-                string absDate = r[7].ToString();
-                string reason = r[8].ToString();
+                foreach (DataRow r in dtAbsentee.Rows)
+                {
+                    string fname = r[1].ToString();
+                    string lname = r[2].ToString();
+                    string tname = r[3].ToString();
+                    string rm = r[4].ToString();
+                    string ginfo = r[5].ToString();
+                    string rptDate = r[6].ToString();
+                    string absDate = r[7].ToString();
+                    string reason = r[8].ToString();
 
-                clsRept rpt = new clsRept(fname, lname, tname, rm, ginfo, rptDate, absDate, reason);
-                rptInfo.Add(rpt);
+                    ClsReport rpt = new ClsReport(fname, lname, tname, rm, ginfo, rptDate, absDate, reason);
+                    rptInfo.Add(rpt);
 
+                }
+
+                dtAbsentee.Clear();
+                dtAbsentee.Dispose();
             }
 
-            dtAbsentee.Clear();
-            dtAbsentee.Dispose();
-        }
+            else
+            {
+                rptInfo = null;
+            }
 
-        else
-        {
-            rptInfo = null;
+            agt.CloseStorageConnection(con);
+            return rptInfo;
         }
-
-        agt.CloseStorageConnection(con);
-        return rptInfo;
     }
-
-}
