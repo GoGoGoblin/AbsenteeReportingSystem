80403fb79de89bf8188f49170ca5f8ab71033fe0
Kathy <penguin.samba.rumba@gmail.com>
2018-03-11 19:26:27 +1300

    Update clsStorageAgent.cs

--- clsStorageAgent.cs
+++ clsStorageAgent.cs
@@ -3,31 +3,28 @@ using System.Data;
 using MySql.Data.MySqlClient;
 using System.Web.Configuration;
 
-public class clsStorageAgent : IStorageAgent
-{
-    public IDbConnection GetStorageConnection()
+    public class ClsStorageAgent : IStorageAgent
     {
-        string conStr = WebConfigurationManager.ConnectionStrings["con"].ConnectionString;
-        return new MySqlConnection(conStr);
-    }
+        public IDbConnection GetStorageConnection()
+        {
+            string conStr = WebConfigurationManager.ConnectionStrings["con"].ConnectionString;
+            return new MySqlConnection(conStr);
+        }
 
-    public void CloseStorageConnection(IDbConnection con)
-    {
-        con.Close();
-        con.Dispose();
-    }
+        public void CloseStorageConnection(IDbConnection con)
+        {
+            con.Close();
+            con.Dispose();
+        }
 
+        public DataTable GetData(IDbConnection con, string strQuery)
+        {
+            var sqlCmd = new MySqlCommand(strQuery, (SqlConnection)con);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            MySqlDataReader clsRdr = sqlCmd.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(clsRdr);
 
-    public DataTable GetData(IDbConnection con, string strQuery)
-    {
-        var sqlCmd = new MySqlCommand(strQuery, (MySqlConnection)con);
-        sqlCmd.CommandType = CommandType.StoredProcedure;
-        MySqlDataReader clsRdr = sqlCmd.ExecuteReader();
-        DataTable dataTable = new DataTable();
-        dataTable.Load(clsRdr);
-
-        return dataTable;
-    }
-
-
-}
+            return dataTable;
+        }
+ }
